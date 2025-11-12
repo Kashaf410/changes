dd97f8cf88d93a4a6f46792870a958da94e7131a
Kashaf410 <Gulrizaansari@gmail.com>
2025-11-12 22:09:43 +0530

    123
    123456

---
+++ for.js
@@ -0,0 +1,19 @@
+for(i=0; i<=10; i++)
+{
+    if(i/2==0)
+        { 
+            continue;
+        }
+    console.log(i);
+}
+
+for(i=0; i<=10; i++)
+{
+    if(i/2==0)
+        { 
+            continue;
+        }
+    console.log(i);
+}
+
+
